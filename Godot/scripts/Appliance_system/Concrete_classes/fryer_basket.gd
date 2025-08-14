e4475ba835fb4a1ead25d3c05b15fd490484bfad
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-08-14 13:10:36 +1200

    Added function to control which direction Placeable is facing to control model #39

--- Godot/scripts/Appliance_system/Concrete_classes/fryer_basket.gd
+++ Godot/scripts/Appliance_system/Concrete_classes/fryer_basket.gd
@@ -4,9 +4,7 @@ extends Cookware
 ## Setup the model instance
 func _init():
 	super._init()
-	model_scene = preload("res://assets/models/furniture/StoveMulti.glb") # for test
-
-
+	model_scene = preload("res://assets/models/items/fryerbasket.glb")
 ## Setup the fryer properties
 func _ready():
 	super._ready()

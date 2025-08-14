2ae3410d88f844c47ad1af56b8c632d82bd8ff01
ringwoemma <ringwoemma@myvuw.ac.nz>
2025-08-14 16:28:23 +1200

    Updated food and stuff to be abstractThrowable

--- Godot/scripts/Food/Ingredients/tomato.gd
+++ Godot/scripts/Food/Ingredients/tomato.gd
@@ -1,12 +1,14 @@
-class_name Tomato
 extends Food
+class_name Tomato
 
 # Sets the variables for tomato that are different to the superclass 
+
+
 func _ready():
 	food_name = "Tomato"
-	raw_mesh = $raw
-	spoiled_mesh = $spoiled
-	cooked_mesh = null
+	raw_mesh = $RawTomato
+	spoiled_mesh = null
+	cooked_mesh = $ChoppedTomato
 	burnt_mesh = null
 	chopped_mesh = null
 	# Spoil time is 80 which is same as super class
@@ -14,4 +16,5 @@ func _ready():
 	# Can cook is true same as super class
 	previous_states=["RAW","CHOPPED", "BOILED"]
 	add_to_group("Food")
+	on_state_change()
 # State of the food changes in the super class

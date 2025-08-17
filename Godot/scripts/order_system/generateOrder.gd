7666727771f723edad29de1cdcdc6c1d532d7521
ringwoemma <ringwoemma@myvuw.ac.nz>
2025-08-17 13:56:34 +1200

    Working on the order system and the plate

---
+++ Godot/scripts/order_system/generateOrder.gd
@@ -0,0 +1,6 @@
+extends Node
+
+# Needs to keep track of the days 
+# The food able to be picked from the menu depends on how far into the game they have gotten
+
+# Do I need to create an enum to do this simply? Or do i have to 

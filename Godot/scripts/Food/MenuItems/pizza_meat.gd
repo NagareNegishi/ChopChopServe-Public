65872e12599349eae138af825b6d954b5abe3cbf
ringwoemma <ringwoemma@myvuw.ac.nz>
2025-08-22 15:10:39 +1200

    Editing menuitems to have different types of items starters, mains, deserts

--- Godot/scripts/Food/MenuItems/pizza_meat.gd
+++ Godot/scripts/Food/MenuItems/pizza_meat.gd
@@ -1,9 +1,11 @@
 extends MenuItem
 class_name MeatPizza
 
-# Need to set ingredient list
-# Need to make sure that the state of ingredients matches the state of the ingredient in the array
+# Registers this class in the correct arrays in the superclass
+@warning_ignore("unused_private_class_variable")
 static var _auto_register = MenuItem.register(MeatPizza)
+@warning_ignore("unused_private_class_variable")
+static var _type_register = MenuItem.register_type(MenuItem.mains, MeatPizza)
 
 func _ready():
 	ingredients = ["Dough", "Cheese", "Tomato", "Beef"]
--- Godot/scripts/Food/MenuItems/steak_and_chips.gd
+++ Godot/scripts/Food/MenuItems/steak_and_chips.gd
@@ -1,10 +1,11 @@
 extends MenuItem
 class_name SteakAndChips
 
-# Need to set ingredient list
-# Need to make sure that the state of ingredients matches the state of the ingredient in the array
-
+# Registers this class in the correct arrays in the superclass
+@warning_ignore("unused_private_class_variable")
 static var _auto_register = MenuItem.register(SteakAndChips)
+@warning_ignore("unused_private_class_variable")
+static var _type_register = MenuItem.register_type(MenuItem.mains, SteakAndChips)
 
 func _ready():
 	ingredients = ["Beef", "Potato"]

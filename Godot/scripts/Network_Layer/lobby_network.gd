7f46fa5b98fa3e8314e84c1ad844a2752e3d2e59
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-08-30 16:48:06 +1200

    template of LobbyNetwork #98

--- Godot/scripts/Network_Layer/lobby_network.gd
+++ Godot/scripts/Network_Layer/lobby_network.gd
@@ -3,8 +3,14 @@ extends Control
 class_name LobbyNetwork
 
 @export var slot_scene: PlayerSlot
-@onready var role_label: Label = %RoleLabel
-@onready var player_list_root: VBoxContainer = %PlayerList
+@onready var role_label: Label = $RoleLabel
+@onready var team_label: Label = $TeamLabel
+@onready var shuffle_button: Button = $ControlContainer/ShuffleButton
+@onready var start_button: Button = $ControlContainer/StartButton
+@onready var leave_button: Button = $ControlContainer/LeaveButton
+@onready var player_list_root: VBoxContainer = $PlayerList
 
-var max_slots: int = 4                            # fallback; will be updated from ENet layer
-var last_players: Array[int] = []
+
+# fallback; will be updated from ENet layer
+var max_slots: int = 4
+var current_players: Array[int] = []

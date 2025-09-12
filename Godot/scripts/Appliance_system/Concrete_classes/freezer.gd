551a2014af728f1cd364d0d81269ceb82a13f137
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-09-12 22:06:34 +1200

    - implemented MultiFoodCrate - made Freezer as one of MultiFoodCrate #67

--- Godot/scripts/Appliance_system/Concrete_classes/freezer.gd
+++ Godot/scripts/Appliance_system/Concrete_classes/freezer.gd
@@ -1,27 +1,8 @@
 class_name Freezer
-extends PoweredAppliance
+extends MultiFoodCrate
 
 ## Setup the model instance
 func _init():
 	super._init()
 	model_scene = preload("res://assets/models/furniture/Fridge.glb")
-
-
-## Setup the freezer properties
-func _ready():
-	super._ready()
-	# cooking_style = ApplianceFactory.CookingStyle.FREEZE
-	valid_classes = ["Pot"] # Only one Pot allowed
-	capacity = 1
-	power = 1
-	_add_cookware("pot")
-
-	# Maybe??
-	# cook_interval = 1.0
-
-
-## Override upgradable setup in concrete appliances
-func _setup_upgradable():
-	super._setup_upgradable()
-	enable_upgrade("power", [1, 1, 1], [100, 200, 300])
-	enable_upgrade("capacity", [1, 1, 1], [80, 160, 240])
+	supply_names = ["Tomato", "Water"]
--- Godot/scripts/Appliance_system/Concrete_classes/multi_food_crate.gd
+++ Godot/scripts/Appliance_system/Concrete_classes/multi_food_crate.gd
@@ -1,22 +1,14 @@
-## Food Crate only provides a one type of ingredient, but takes nothing back.
-## player can take food from food crate manually,
-##
-## Note: the implementation can be simplify by extending Appliance directly,
-## however, considering the future extension, I will extend UnPoweredAppliance
-class_name FoodCrate
+## MultiFoodCrate provides multiple types of ingredient, but takes nothing back.
+## player can take food from food crate manually
+class_name MultiFoodCrate
 extends UnPoweredAppliance
 
 @export_group("Supply Settings")
-@export var supply: PackedScene
-@export var supply_name: String = "Tomato" # "Water"
+@export var supplies: Array[PackedScene] = []
+@export var supply_names: Array[String] = ["Tomato"]
 var food_directory: String = "res://scripts/Food/IngredientScenes/"
-var supply_instance: Node
-
-
-## Setup the model instance
-func _init():
-	super._init()
-	model_scene = preload("res://assets/models/NuFurniture/FoodCrater.glb")
+var supply_instances: Array[Node] = []
+var supply_counts: Array[int] = []
 
 
 ## Set up the FoodCrate
@@ -30,83 +22,126 @@ func _ready():
 ## Add synchronization properties for the placeable object
 func _add_sync_properties(config: SceneReplicationConfig):
 	super._add_sync_properties(config)
-	config.add_property(NodePath(".:supply_count"))
+	config.add_property(NodePath(".:supply_counts"))
 
 
 ## Initialize the supply
+## Support people prefer configuring through .tscn
 func _initialize_supply():
-	if supply and supply.can_instantiate():
-		supply_instance = supply.instantiate()
+	# case no supplies configured
+	if supplies.is_empty():
+		set_supply(supply_names)
 		return
-	set_supply(supply_name)
+
+	# verify configured supplies
+	var is_configured = true
+	for supply in supplies:
+		if supply and supply.can_instantiate():
+			var instance = supply.instantiate()
+			supply_instances.append(instance)
+			supply_counts.append(1)
+		else:
+			is_configured = false
+	if is_configured:
+		return
+	
+	# default behavior
+	push_error("Supplies not properly configured!!, overriding supplies with supply_names")
+	supplies.clear()
+	supply_instances.clear()
+	supply_counts.clear()
+	set_supply(supply_names)
 
 
-# Set the supply script for the food crate
-func set_supply(food_name: String):
-	var scene_path = food_directory + food_name + ".tscn"
-	supply = load(scene_path)
-	if supply and supply.can_instantiate():
-		supply_instance = supply.instantiate()
-		# print("FoodCrate supply set to: ", scene_path.get_file().get_basename())
-	else:
-		push_error("Failed to load or cannot instantiate scene: " + scene_path)
+## Set the supply script for the food crate
+func set_supply(food_names: Array[String]):
+	for food_name in food_names:
+		var scene_path = food_directory + food_name + ".tscn"
+		var supply_scene = load(scene_path)
+		if supply_scene and supply_scene.can_instantiate():
+			supplies.append(supply_scene)
+			supply_instances.append(supply_scene.instantiate())
+			supply_counts.append(1)
+		else:
+			push_error("Failed to load or cannot instantiate scene: " + scene_path)
 
 
 ## Provide food from the crate
+## @param index: The index of the food item to take
 ## @return: The food item that was taken, or null if not in IDLE status
-func take() -> Node:
+func take_at(index: int) -> Node:
 	if current_status != Status.IDLE:
 		push_error("FoodCrate is not in IDLE status, cannot take food")
 		return null
+	if index < 0 or index >= supply_instances.size():
+		push_error("Index out of range in take_at: " + str(index))
+		return null
 	current_status = Status.USING
 	action_timer.start()
-
-	var food = supply.instantiate()
-	food.name = prefix + supply_name + str(supply_count)
-	supply_count += 1
+	var food = supplies[index].instantiate()
+	if not food:
+		push_error("Failed to instantiate food at index: " + str(index))
+		current_status = Status.IDLE
+		return null
+	food.name = prefix + supply_names[index] + str(supply_counts[index])
+	supply_counts[index] += 1
 	ApplianceManager.register_item(food, current_owner, food.name)
 	return food
 
 
 ## For Player interaction --------------------------------------------------------------------------
 
+#TODO: player_has should take index as well
+
 ## Perform action depend on what player is holding
 ## @param _item: The Node Player is holding
 ## @return: True if action is triggered, false otherwise
 func player_has(item: Node) -> void:
+	print("pop up UI here")
+	player_selected(item, 0)
+
+
+## Perform action depend on what player is holding
+## @param _item: The Node Player is holding
+## @param index: The index of the food item to take
+## @return: True if action is triggered, false otherwise
+func player_selected(item: Node, index: int) -> void:
 	if not item:
-		take_request()
+		take_at_request(index)
 		return
 
-	if item is Cookware and item._can_accept(supply_instance):
-		transfer_request(item)
+	if item is Cookware and item._can_accept(supply_instances[index]):
+		transfer_request(item, index)
 		return
 
 
 ## Client-side method to take item, called by host
 ## @param item_name: The name of the item to take
 @rpc("authority", "call_remote", "reliable")
-func _client_take(item_name: String) -> void:
-	var item = take()
+func _client_take_at(item_name: String, index: int) -> void:
+	var item = take_at(index)
+	if not item:
+		push_error("Failed to take item at index: " + str(index))
+		return
 	item.name = item_name
 	get_tree().current_scene.add_child(item)
 
 
 ## Request to take an item from this appliance to Player
-func take_request() -> void:
-	_take_as_host.rpc_id(1, ENetManager.get_my_id())
+func take_at_request(index: int) -> void:
+	_take_at_as_host.rpc_id(1, ENetManager.get_my_id(), index)
 
 
 ## Host-side method to handle take requests from clients
 ## @param player_id: The id of the player who is taking the item
 @rpc("any_peer", "call_local", "reliable")
-func _take_as_host(player_id: int) -> void:
+func _take_at_as_host(player_id: int, index: int) -> void:
 	if not ENetManager.is_host():
 		return
 	# host need check to prevent conflicts/ cheating
-	var item = take()
+	var item = take_at(index)
 	get_tree().current_scene.add_child(item)
-	_client_take.rpc(item.name)
+	_client_take_at.rpc(item.name, index)
 	_give_item_to_player.rpc(player_id, item.get_path())
 
 
@@ -124,37 +159,37 @@ func _give_item_to_player(player_id: int, item_path: NodePath) -> void:
 
 ## Transfer food from FoodCrate to Cookware
 ## @param cookware: The Cookware to transfer food to
-func transfer_request(player_cookware: Cookware) -> void:
-	var food = take()
+func transfer_request(player_cookware: Cookware, index: int) -> void:
+	var food = take_at(index)
 	if not food:
 		return
 	if ENetManager.is_host():
 		player_cookware._put(food)
-		_client_transfer.rpc(ENetManager.get_my_id(), food.name)
+		_client_transfer.rpc(ENetManager.get_my_id(), food.name, index)
 		return
-	_transfer_as_host.rpc_id(1, ENetManager.get_my_id())
+	_transfer_as_host.rpc_id(1, ENetManager.get_my_id(), index)
 
 
 ## Host-side method to handle transfer requests from clients
 ## @param player_id: The id of the player who is transferring the food
 @rpc("any_peer", "call_remote", "reliable")
-func _transfer_as_host(player_id: int) -> void:
+func _transfer_as_host(player_id: int, index: int) -> void:
 	if not ENetManager.is_host():
 		return
 	var player_cookware = GlobalScript.get_local_player_by_id(player_id).item_in_hand
-	if player_cookware is Cookware and player_cookware._can_accept(supply_instance):
-		var food = take()
+	if player_cookware is Cookware and player_cookware._can_accept(supply_instances[index]):
+		var food = take_at(index)
 		player_cookware._put(food)
-		_client_transfer.rpc(player_id, food.name)
+		_client_transfer.rpc(player_id, food.name, index)
 
 
 ## Client-side method to transfer food, called by host
 ## @param player_id: The id of the player who is transferring the food
 @rpc("authority", "call_remote", "reliable")
-func _client_transfer(player_id: int, food_name: String) -> void:
+func _client_transfer(player_id: int, food_name: String, index: int) -> void:
 	var player_cookware = GlobalScript.get_local_player_by_id(player_id).item_in_hand
-	if player_cookware is Cookware and player_cookware._can_accept(supply_instance):
-		var food = take()
+	if player_cookware is Cookware and player_cookware._can_accept(supply_instances[index]):
+		var food = take_at(index)
 		food.name = food_name
 		player_cookware._put(food)
 
@@ -174,10 +209,10 @@ func _on_interactable_component_hovered(is_hovered: bool) -> void:
 		highlight_component.show_feedback(true)
 		return
 	if item is Cookware:
-		if item._can_accept(supply_instance):
-			highlight_component.show_feedback(true)
-		else:
+		if item.is_full():
 			highlight_component.show_feedback(false)
+		else:
+			highlight_component.show_feedback(true)
 #---------------------------------------------------------------------------------------------------
 
 
@@ -186,8 +221,8 @@ func put(_item: Node) -> bool:
 	assert(false, "Food Crate does not support putting items")
 	return false
 
-func take_at(_index: int) -> Node:
-	assert(false, "Food Crate does not support taking items at specific index")
+func take() -> Node:
+	assert(false, "MultiFoodCrate requires an index to take specific food item")
 	return null
 
 func start_action() -> bool:

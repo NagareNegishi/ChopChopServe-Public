2664ded2a28c297bf6ad3a77a7a10d971b190590
ExtraStrawberryMilk <132359683+ExtraStrawberryMilk@users.noreply.github.com>
2025-10-07 16:04:28 +1300

    #153 UI for rercipes and loading screen

--- Godot/scripts/Food/Ingredients/chicken.gd
+++ Godot/scripts/Food/Ingredients/chicken.gd
@@ -8,7 +8,7 @@ func _ready():
 	spoiled_mesh = $SpoiledChicken
 	cooked_mesh = $ChickenCooked
 	burnt_mesh = $BurntChicken
-	texture = load("res://scripts/Food/Ingredients/chicken.png")
+	#texture = load("res://scripts/Food/Ingredients/chicken.png")
 	
 	add_to_group("Food")
 	on_state_change()

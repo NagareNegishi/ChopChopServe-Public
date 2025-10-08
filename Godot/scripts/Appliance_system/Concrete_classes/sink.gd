094e9af5c7e5dab2cfb1cf400e7e45abba7c6dde
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-10-08 21:13:39 +1300

    refactor sink, comment out providing water function #169

--- Godot/scripts/Appliance_system/Concrete_classes/sink.gd
+++ Godot/scripts/Appliance_system/Concrete_classes/sink.gd
@@ -2,7 +2,6 @@
 class_name Sink
 extends UnPoweredAppliance
 
-var water_scene: PackedScene = preload("res://scripts/Food/IngredientScenes/Water.tscn")
 var bubble_particles: ParticleController
 
 ## Setup the model instance
@@ -17,15 +16,11 @@ func _ready():
 	super._ready()
 	capacity = 1
 	_setup_visual_effects()
-	_set_affixes()
-	if not (water_scene and water_scene.can_instantiate()):
-		push_error("Failed to preload water scene in Sink")
-
-
-## Add synchronization properties for the placeable object
-func _add_sync_properties(config: SceneReplicationConfig):
-	super._add_sync_properties(config)
-	config.add_property(NodePath(".:supply_count"))
+	# see the NOTE: 8/10/2025 below --------------------------------------------
+	# _set_affixes()
+	# if not (water_scene and water_scene.can_instantiate()):
+	# 	push_error("Failed to preload water scene in Sink")
+	# --------------------------------------------------------------------------
 
 
 ## Add interactable component to this class
@@ -61,15 +56,6 @@ func _toggle_bubble(bubble: bool) -> void:
 ## @return: True if washing started
 func wash() -> bool:
 	return start_action()
-
-
-## Provide water, register it with unique name
-## @return: The Water instance provided
-func _provide_water() -> Water:
-	var water = water_scene.instantiate()
-	water.name = prefix + "Water" + str(supply_count)
-	supply_count += 1
-	return water
 
 
 ## Check if this appliance can accept the given item
@@ -112,59 +98,18 @@ func player_has(item: Node) -> void:
 	if not item:
 		take_request()
 		return
-	# If Player has Pot, provide water
-	if item is Pot:
-		supply_water_request(item)
-		return
+	# see the NOTE: 8/10/2025 below --------------------------------------------
+	# # If Player has Pot, provide water
+	# if item is Pot:
+	# 	supply_water_request(item)
+	# 	return
+	# --------------------------------------------------------------------------
 	# If player has empty plate: depend on if sink can accept it
 	put_request(item)
 
 
-## Supply water from Sink to Pot
-## @param pot: The Pot to supply water to
-func supply_water_request(pot: Pot) -> void:
-	# locally check first to reduce network calls
-	if pot.is_full():
-		return
-	if ENetManager.is_host():
-		var water = _provide_water()
-		pot._put(water)
-		_client_supply_water.rpc(ENetManager.get_my_id(), water.name)
-		pot._sync_contents.rpc(pot.contents_names)
-		return
-	_supply_water_as_host.rpc_id(1, ENetManager.get_my_id())
-
-
-## Host-side method to handle water supply requests from clients
-## @param player_id: The id of the player who is requesting water
-@rpc("any_peer", "call_remote", "reliable")
-func _supply_water_as_host(player_id: int) -> void:
-	if not ENetManager.is_host():
-		return
-	var pot = GlobalScript.get_local_player_by_id(player_id).item_in_hand
-	if not pot or not (pot is Pot) or pot.is_full():
-		return
-	var water = _provide_water()
-	pot._put(water)
-	_client_supply_water.rpc(player_id, water.name)
-	pot._sync_contents.rpc(pot.contents_names)
-
-
-## Client-side method to supply water, called by host
-## @param player_id: The id of the player who requested water
-## @param water_name: The unique name of the water item
-@rpc("authority", "call_remote", "reliable")
-func _client_supply_water(player_id: int, water_name: String) -> void:
-	var pot = GlobalScript.get_local_player_by_id(player_id).item_in_hand
-	if pot and pot is Pot and not pot.is_full():
-		var water = water_scene.instantiate() # Skip registration, already done by host
-		water.name = water_name
-		pot._put(water)
-
-
 ## Trigger action, if subclass has action
 func _on_interactable_component_action_use(_is_action: bool) -> void:
-	print("Player used action on: ", get_script().get_global_name(), ", it can wash.")
 	if _is_action:
 		wash()
 		_toggle_bubble(true)
@@ -185,6 +130,65 @@ func _on_interactable_component_hovered(is_hovered: bool) -> void:
 	if not item:
 		highlight_component.set_state(ApplianceHighlight.HighlightState.HOVER)
 		return
-	var can_accept = _can_accept(item) or (item is Pot and not item.is_full())
+	var can_accept = _can_accept(item) # or (item is Pot and not item.is_full())
 	highlight_component.show_feedback(can_accept)
 #---------------------------------------------------------------------------------------------------
+
+
+## NOTE: 8/10/2025
+# the team removed water from cooking, therefore Sink no longer needs to supply water to Pot.
+# However, I will keep the water supply code here for future reference, in case we want to reintroduce it. -------------
+
+# var water_scene: PackedScene = preload("res://scripts/Food/IngredientScenes/Water.tscn")
+
+# ## Add synchronization properties for the placeable object
+# func _add_sync_properties(config: SceneReplicationConfig):
+# 	super._add_sync_properties(config)
+# 	config.add_property(NodePath(".:supply_count"))
+
+# ## Provide water, register it with unique name
+# ## @return: The Water instance provided
+# func _provide_water() -> Water:
+# 	var water = water_scene.instantiate()
+# 	water.name = prefix + "Water" + str(supply_count)
+# 	supply_count += 1
+# 	return water
+
+# ## Supply water from Sink to Pot
+# ## @param pot: The Pot to supply water to
+# func supply_water_request(pot: Pot) -> void:
+# 	# locally check first to reduce network calls
+# 	if pot.is_full():
+# 		return
+# 	if ENetManager.is_host():
+# 		var water = _provide_water()
+# 		pot._put(water)
+# 		_client_supply_water.rpc(ENetManager.get_my_id(), water.name)
+# 		pot._sync_contents.rpc(pot.contents_names)
+# 		return
+# 	_supply_water_as_host.rpc_id(1, ENetManager.get_my_id())
+
+# ## Host-side method to handle water supply requests from clients
+# ## @param player_id: The id of the player who is requesting water
+# @rpc("any_peer", "call_remote", "reliable")
+# func _supply_water_as_host(player_id: int) -> void:
+# 	if not ENetManager.is_host():
+# 		return
+# 	var pot = GlobalScript.get_local_player_by_id(player_id).item_in_hand
+# 	if not pot or not (pot is Pot) or pot.is_full():
+# 		return
+# 	var water = _provide_water()
+# 	pot._put(water)
+# 	_client_supply_water.rpc(player_id, water.name)
+# 	pot._sync_contents.rpc(pot.contents_names)
+
+# ## Client-side method to supply water, called by host
+# ## @param player_id: The id of the player who requested water
+# ## @param water_name: The unique name of the water item
+# @rpc("authority", "call_remote", "reliable")
+# func _client_supply_water(player_id: int, water_name: String) -> void:
+# 	var pot = GlobalScript.get_local_player_by_id(player_id).item_in_hand
+# 	if pot and pot is Pot and not pot.is_full():
+# 		var water = water_scene.instantiate() # Skip registration, already done by host
+# 		water.name = water_name
+# 		pot._put(water)

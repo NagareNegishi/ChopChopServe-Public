b01ea2d6a73dfb7a01c3dfcd2120d33d5f1bc986
Johnathan Devine <johnodevine@gmail.com>
2025-10-15 15:43:28 +1300

    Added controller stuff to food factoryt

--- Godot/scripts/Appliance_system/Concrete_classes/food_factory.gd
+++ Godot/scripts/Appliance_system/Concrete_classes/food_factory.gd
@@ -243,7 +243,7 @@ func _on_interactable_component_action_use(_is_action: bool) -> void:
 	if !_is_action: return
 	inventory_sprite.inventory.current_slot = inventory_sprite.inventory.move_forward()
 	inventory_sprite.inventory.update_slot_selected(true)
-	
+
 
 ## Override unsupported methods to prevent misuse ------------------------------
 func put(_item: Node) -> bool:
@@ -255,7 +255,6 @@ func take() -> Node:
 	return null
 
 func player_has(_item: Node) -> void:
-	print("MEOOWWWWWWWW")
 	inventory_sprite.inventory.select_ingredient()
 	return
 
@@ -263,3 +262,12 @@ func put_from_player(_item: Node) -> bool:
 	assert(false, "Food Crate does not support putting items")
 	return false
 #-------------------------------------------------------------------------------
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("LB"):
+		inventory_sprite.inventory.current_slot = inventory_sprite.inventory.move_backward()
+		inventory_sprite.inventory.update_slot_selected(true)
+	if Input.is_action_just_pressed("RB"):
+		inventory_sprite.inventory.current_slot = inventory_sprite.inventory.move_forward()
+		inventory_sprite.inventory.update_slot_selected(true)
